6cdfb5c887b817f9cf8d56ed75b766b74cbf0648
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-02-22 22:30:17 +0530

    Implemented readFile
    Implemented readFile which reads the give file and stores its content.
    It can throw exceptions for file not found and reading errors.

---
+++ src/ReadFile.java
@@ -0,0 +1,52 @@
+import java.io.*;
+
+class ReadFile {
+
+	private String fileName, content;
+	FileInputStream file;
+
+	private ReadFile(String file){
+		this.fileName = file;
+		this.file = null;
+		this.content = "";
+	}
+
+	static ReadFile addFile(String file){
+		ReadFile self = new ReadFile(file);	
+		try{
+			self.addInputStream();
+			self.read();
+		}
+		catch(FileNotFoundException err){
+			System.out.println("ERROR.... File Not Found");
+		}
+		catch(IOException err){
+			System.out.println("Oops... The File can't be read...");
+		}
+		return self;
+	}
+
+	private void addInputStream() throws FileNotFoundException{
+		this.file = new FileInputStream(this.fileName);
+	}
+
+	private void read() throws IOException{
+		char c[];
+		int temp;
+		String text;
+		while((temp = this.file.read())!=-1){
+			c = Character.toChars(temp);
+			text = new String(c);
+			this.content = this.content.concat(text);
+		}
+	}
+
+	public String getContent(){
+		return this.content;
+	}
+
+	public String getFileName(){
+		return this.fileName;
+	}
+	
+}
