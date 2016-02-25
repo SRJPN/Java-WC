026f1a929859ce8169d3380ca4b2ca2c792f882a
Sooraj Parameswaran <soorajp@thoughtworks.com>
2016-02-25 17:20:17 +0530

    Added some Misc files
    Added some miscellaneous files which are part of intelliJ and git.

--- src/Anu.java
+++ src/Anu.java
@@ -4,44 +4,43 @@ import java.io.UnsupportedEncodingException;
 
 class Anu {
 
-	String content;
+    String content;
 
-	public Anu(String content){
-		this.content = content;
-	}
-	
-	public int charCount(){
-		return this.content.length();
-	}
+    public Anu(String content) {
+        this.content = content;
+    }
 
-	public int lineCount(){
-		int occurrences = 0;
-		for(char c : this.content.toCharArray()){
-		   if(c == '\n'){
-		      occurrences++;
-		   }
-		}
-		return occurrences;
-	}
+    public int charCount() {
+        return this.content.length();
+    }
 
-	public int byteCount(){
-		byte[] utf8Bytes = {};
-		try{
-			utf8Bytes = this.content.getBytes("UTF-8");
-		}
-		catch(UnsupportedEncodingException err){
-			System.out.println("ERROR.....!");
-		}
-		return utf8Bytes.length;
-	}
+    public int lineCount() {
+        int occurrences = 0;
+        for (char c : this.content.toCharArray()) {
+            if (c == '\n') {
+                occurrences++;
+            }
+        }
+        return occurrences;
+    }
 
-	public int wordCount(){
-		int occurrences = 0;
-    	String pattern = "\\w+";
-    	Pattern r = Pattern.compile(pattern);
-    	Matcher m = r.matcher(this.content);
-    	while(m.find())
-    		occurrences++;
-    	return occurrences;
-	}
+    public int byteCount() {
+        byte[] utf8Bytes = {};
+        try {
+            utf8Bytes = this.content.getBytes("UTF-8");
+        } catch (UnsupportedEncodingException err) {
+            System.out.println("ERROR.....!");
+        }
+        return utf8Bytes.length;
+    }
+
+    public int wordCount() {
+        int occurrences = 0;
+        String pattern = "\\w+";
+        Pattern r = Pattern.compile(pattern);
+        Matcher m = r.matcher(this.content);
+        while (m.find())
+            occurrences++;
+        return occurrences;
+    }
 }
--- src/WC.java
+++ src/WC.java
@@ -1,11 +1,35 @@
+import org.apache.commons.cli.CommandLine;
 
 class WC {
 	public static void main(String[] args) {
-		for(String file : args){
-			ReadFile text = ReadFile.addFile(file);
+		Cli cli = new Cli(args);
+		CommandLine cmds;
+		try{
+			cmds = cli.parse();
+			String[] files = cmds.getArgs();
+			String[] resultEach = processFiles(files, cmds);
+			for(String s : resultEach)
+				System.out.println(s);
+		}
+		catch (Exception e){
+			cli.help();
+			System.exit(0);
+		}
+	}
+
+	private static String[] processFiles(String[] files, CommandLine options){
+		String[] result = new String[files.length];
+		for(int i=0; i<files.length; i++){
+			ReadFile text = ReadFile.addFile(files[i]);
 			String content = text.getContent();
 			Anu anu = new Anu(content);
-			System.out.println("\t"+anu.lineCount()+"\t"+anu.wordCount()+"\t"+anu.charCount()+"\t"+file);
+			
+			result[i] = processFile(anu, options)+" "+files[i];
 		}	
+		return result;
+	}
+
+	private static String processFile(Anu anu, CommandLine options){
+			return "\t"+anu.lineCount()+"\t"+anu.wordCount()+"\t"+anu.charCount();
 	}
 }
